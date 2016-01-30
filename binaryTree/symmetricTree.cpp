34e5f0154b67bb7713a4a0abad33c8b6b09b6a2f
keqhe <keqhe@cs.wisc.edu>
2016-01-30 17:51:37 -0600

    symmetric binary tree

---
+++ binaryTree/symmetricTree.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<string>
+#include<queue>
+#include<vector>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	struct TreeNode * left;
+	struct TreeNode * right;
+	TreeNode(int x): val(x), left(NULL), right(NULL) {};
+};
+
+class Solution {
+public:
+    bool helper(struct TreeNode* l, struct TreeNode* r) {
+        if (!l && !r) //all are NULL
+            return true;
+        else if (!l || !r)//only 1 sub-tree is NULL
+            return false;
+        else if (l->val != r->val)
+            return false;
+        else {
+            return helper(l->left, r->right) && helper(l->right, r->left);
+        }
+    }
+    bool isSymmetric_idea1(TreeNode* root) {
+        if (!root)
+            return true;
+        return helper(root->left, root->right);
+    }
+    //another method is BFS, or level-order traversal
+    //ref: https://leetcode.com/discuss/79348/iterative-java-solution-with-explanation
+    bool isSymmetric(struct TreeNode * root) {
+        if (!root)  
+            return true;
+        queue<struct TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            vector<struct TreeNode*> tmp;
+            int size = q.size();
+            for (int i = 0; i < size; i ++) {
+                struct TreeNode * p = q.front();
+                tmp.push_back(p);
+                q.pop();
+                if (p) {
+                    q.push(p->left);
+                    q.push(p->right);
+                }
+            }
+            
+            int i = 0;
+            int j = tmp.size() - 1;
+            //cout << i << " " << j << endl;
+            while (i < j) {
+                if (!tmp[i] && !tmp[j]) {
+                    i ++;
+                    j --;
+                }
+                else if (!tmp[i] || !tmp[j])
+                    return false;
+                else if (tmp[i] && tmp[j]) {
+                    if (tmp[i]->val != tmp[j]->val)
+                        return false;
+                    i ++;
+                    j --;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+	struct TreeNode * p = new TreeNode(1);
+	class Solution sn;
+	cout << sn.isSymmetric(p) << endl;
+	delete p;
+	return 0;
+}
