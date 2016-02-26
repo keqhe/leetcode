94119815eb1a611b954a245ce20154916a927ea4
keqhe <keqhe@cs.wisc.edu>
2016-02-26 12:55:10 -0600

    161. One Edit Distance

---
+++ string/leetcode161.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    //add a char, delete a char, modify a char
+    bool isOneEditDistance(string s, string t) {
+        if (s.length() == t.length()) {
+            for (int i = 0; i < s.length(); i ++) {
+                if (s[i] != t[i]) {
+                    return s.substr(i+1) == t.substr(i+1); 
+                }
+            }
+            return false;
+        }
+        else if (s.length() + 1 == t.length()) {
+            for (int i = 0; i < s.length(); i ++) {
+                if (s[i] != t[i]) {
+                    return s.substr(i) == t.substr(i+1);
+                }
+            }
+            return true;
+        }
+        else if (t.length() + 1 == s.length())
+            return isOneEditDistance(t, s);
+        else
+            return false;
+    }
+};
+
+int main() {
+	class Solution sn;
+	return 0;
+}
