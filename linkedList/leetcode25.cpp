d290e8f2a6ae04bbb2dea2c346ad104767d91737
keqhe <keqhe@cs.wisc.edu>
2016-02-26 11:11:07 -0600

    25. Reverse Nodes in k-Group

---
+++ linkedList/leetcode25.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<deque>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	struct ListNode * next;
+	ListNode(int x) {
+		val = x;
+		next = NULL;
+	}
+};
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head || !head->next || k == 1)
+            return head;
+        deque<struct ListNode*> de;
+        struct ListNode * curr = head;
+        struct ListNode * dummy = new ListNode(0);
+        struct ListNode * p = dummy;
+        while (curr) {
+            if (de.size() < k) {
+                de.push_back(curr);
+            }
+            else {
+                //have k points in deque now
+                while (!de.empty()) {
+                    p->next = de.back();
+                    de.pop_back();
+                    p = p->next;
+                }
+                de.push_back(curr);
+            }
+            curr = curr->next;
+        }
+        //leftover
+        if (de.size() < k) {
+            while (!de.empty()) {
+                p->next = de.front();
+                de.pop_front();
+                p = p->next;
+            }
+        }
+        else {
+            //have k points in deque now
+            while (!de.empty()) {
+                p->next = de.back();
+                de.pop_back();
+                p = p->next;
+            }
+        }
+        
+        p->next = NULL;
+        
+        struct ListNode * res = dummy->next;
+        delete dummy;
+        return res;
+    }
+};
+
+int main() {
+	class Solution sn;
+	return 0;
+}
