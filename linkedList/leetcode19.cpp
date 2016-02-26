a16bd1456d9c3b12d6367384a4c6b8ce8be9c09f
keqhe <keqhe@cs.wisc.edu>
2016-02-25 23:36:22 -0600

    19. Remove Nth Node From End of List

---
+++ linkedList/leetcode19.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+struct ListNode {
+	int val;	
+	struct ListNode * next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    //two pointers, ref: https://leetcode.com/discuss/87494/from-wrong-to-ac-c-implementation
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        struct ListNode * dummy = new ListNode(0);
+        dummy->next = head;
+        struct ListNode * ahead = dummy;
+        struct ListNode * behind = dummy;
+        for (int i = 0; i <=n; i ++)
+            ahead = ahead->next;
+        //now ahead - behind = n
+        while (ahead) {
+            behind = behind->next;
+            ahead = ahead->next;
+        }
+        struct ListNode * tmp = behind->next;
+        behind->next = behind->next->next;
+        delete tmp;
+        struct ListNode * res = dummy->next;
+        delete dummy;
+        return res;
+    }
+};
+
+int main() {
+	class Solution sn;
+	return 0;
+}
