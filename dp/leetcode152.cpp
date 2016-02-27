9732e857446fe52631a8718946c12ed20baed69c
keqhe <keqhe@cs.wisc.edu>
2016-02-27 04:04:06 -0600

    152. Maximum Product Subarray

---
+++ dp/leetcode152.cpp
@@ -0,0 +1,34 @@
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    //dp_max[i] = max(dp_max[i-1]*nums[i], dp_min[i-1]*nums[i], nums[i])
+    //dp_min[i] = min(dp_max[i-1]*nums[i], dp_min[i-1]*nums[i], nums[i])
+    int maxProduct(vector<int>& nums) {
+        if (nums.empty())
+            return 0;
+        int len = nums.size();
+        vector<int> dp_max(len, 0);
+        vector<int> dp_min(len, 0);
+        
+        dp_max[0] = nums[0];
+        dp_min[0] = nums[0];
+        
+        int Max = dp_max[0];
+        for (int i = 1; i < len; i ++) {
+            dp_max[i] = max(max(dp_max[i-1]*nums[i], dp_min[i-1]*nums[i]), nums[i]);
+            dp_min[i] = min(min(dp_max[i-1]*nums[i], dp_min[i-1]*nums[i]), nums[i]);
+            
+            Max = max(Max, dp_max[i]);
+        }
+        
+        return Max;
+    }
+};
+
+int main() {
+	class Solution sn;
+	return 0;
+}
