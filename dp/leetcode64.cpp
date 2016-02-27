c8d0684a88f81edecb8da6fe42729756f9e82327
keqhe <keqhe@cs.wisc.edu>
2016-02-27 02:26:17 -0600

    64. Minimum Path Sum

---
+++ dp/leetcode64.cpp
@@ -0,0 +1,38 @@
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        if (grid.empty() || grid[0].empty())
+            return 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i ++) {
+            if (i > 0)
+                dp[i][0] = dp[i-1][0] + grid[i][0];
+            else if (i == 0)
+                dp[i][0] = grid[i][0];
+        }
+        for (int j = 0; j < n; j ++) {
+            if (j > 0)
+                dp[0][j] = dp[0][j-1] + grid[0][j];
+            else if (j == 0)
+                dp[0][j] = grid[0][j];
+        }
+        for (int i = 1; i < m; i ++) {
+            for (int j = 1; j < n; j ++) {
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+            }
+        }
+        
+        return dp[m-1][n-1];
+    }
+};
+
+int main() {
+	class Solution sn;
+	return 0;
+}
