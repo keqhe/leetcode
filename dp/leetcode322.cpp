c4752f0b5cae9a1750203a9e9de67fe67ec71497
keqhe <keqhe@cs.wisc.edu>
2016-02-26 22:00:27 -0600

    322. Coin Change

---
+++ dp/leetcode322.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    //dp[i] = dp[i-j] + 1;
+    int coinChange(vector<int>& coins, int amount) {
+        if (coins.empty() || amount == 0)
+            return 0;
+        vector<int> dp(amount+1, amount+1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i ++) {
+            for (auto x : coins) {
+                if (i >= x) {
+                    dp[i] = min(dp[i], dp[i-x]+1);
+                }
+            }
+        }
+        /*for (int i = 0; i <= amount; i ++)
+            cout << dp[i] << " ";
+        cout << endl;
+        */
+        return (dp[amount] == amount+1 ? -1 : dp[amount]);
+    }
+};
+
+int main() {
+	class Solution sn;
+	return 0;
+}
