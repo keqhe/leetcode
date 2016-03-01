e94bb6b297753d9046571f423ea22b6acf7ffcd8
keqhe <keqhe@cs.wisc.edu>
2016-02-29 22:30:37 -0600

    190. Reverse Bits

---
+++ bit_operator/leetcode190.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        unsigned int res = 0;
+        for (int i = 0; i < 32; i ++) {
+            int t = n & 1;
+            cout << t << " ";
+            n = n >> 1;
+            res += t;
+            if (i != 31)
+                res = res << 1;
+            cout << res << endl;
+        }
+        cout << endl;
+        return res;
+    }
+};
+
+int main() {
+	class Solution sn;
+	return 0;
+}
