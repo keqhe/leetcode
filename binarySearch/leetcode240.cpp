ae82b370de4f661d2ace2ba0c7578c62e37b38b2
keqhe <keqhe@cs.wisc.edu>
2016-03-05 10:41:36 -0600

    240. Search a 2D Matrix II

---
+++ binarySearch/leetcode240.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    //sometimes we want to find the position that is just smaller than the target
+    //a common requirement in leetcode binary search problems
+    
+    //well, this problem has a better solution
+    //see ref: https://leetcode.com/discuss/83239/clean-c-implementation-with-detailed-complexity-analysis
+    //key idea: top right coner can help us exclude a row or column
+    //time complexity is O(m+n)
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.empty() || matrix[0].empty())
+            return false;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        int i = 0;
+        int j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target)
+                return true;
+            else if (matrix[i][j] > target)
+                j --;
+            else 
+                i ++;
+        }
+        return false;
+        
+    }
+};
+
+int main() {
+	class Solution sn;
+	return 0;
+}
