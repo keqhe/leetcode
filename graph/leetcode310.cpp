f158b589695bdde3fc1f35198f49b5c5103237d5
keqhe <keqhe@cs.wisc.edu>
2016-03-05 03:16:35 -0600

    310. Minimum Height Trees

---
+++ graph/leetcode310.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    //a nice ref: http://algobox.org/minimum-height-trees/
+    vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
+        
+        vector<int> res;
+        if (n == 1) {
+            res.push_back(0);
+            return res;
+        }
+        
+        vector<unordered_set<int>> adj (n,unordered_set<int>());
+        for (auto e : edges) {
+            int a = e.first;
+            int b = e.second;
+            adj[a].insert(b);
+            adj[b].insert(a);
+        }
+        vector<int> leaves;
+        for (int i = 0; i < n; i ++) {
+            if (adj[i].size() == 1)
+                leaves.push_back(i);
+        }
+        int total = n;
+        while (total > 2) {
+            total -= leaves.size();
+            vector<int> newleaves;
+            for (auto x : leaves) {
+                int y = *(adj[x].begin());
+                adj[y].erase(x);
+                if (adj[y].size() == 1)
+                    newleaves.push_back(y);
+            }
+            leaves = newleaves;
+        }
+        
+        return leaves;
+        
+        return res;
+    }
+};
+
+int main() {
+	class Solution sn;
+	return 0;
+}
