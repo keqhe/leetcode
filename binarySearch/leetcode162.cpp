95dc9fa7f039e6ee3e465da9cfc024d61b39e101
keqhe <keqhe@cs.wisc.edu>
2016-03-07 23:07:11 -0600

    162. Find Peak Element

---
+++ binarySearch/leetcode162.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        if (nums.size() == 1)
+            return 0;
+        int left = 0;
+        int right = n-1;
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            if (mid > 0 && mid < n-1) {
+                if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
+                    return mid;
+                else if (nums[mid] > nums[mid-1]) {
+                    left = mid+1;
+                }
+                else
+                    right = mid-1; 
+            }
+            else if (mid == 0) {
+                if (nums[mid] > nums[mid+1])
+                    return mid;
+                else
+                    left = mid+1;
+            }
+            else if (mid == n - 1) {
+                if (nums[mid] > nums[mid-1])
+                    return mid;
+                else
+                    right = mid - 1;
+            }
+        }
+    }
+};
+
+int main() {
+	class Solution sn;
+	return 0;
+}
