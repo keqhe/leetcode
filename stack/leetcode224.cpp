91eef76ac10ea80ea1af0330581bd1a60d08c95e
keqhe <keqhe@cs.wisc.edu>
2016-03-12 12:25:56 -0600

    224. Basic Calculator

---
+++ stack/leetcode224.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+
+using namespace std;
+
+class Solution {
+public:
+    //string, isdigit, isalpha, isalnum
+    int calculate(string s) {
+        stack<int> st;
+        int flag = 1;
+        int i = 0;
+        int res = 0;
+        for (i = 0; i < s.length(); i ++) {
+            if (isdigit(s[i])) {
+                int sum = s[i] - '0';
+                while (i+1 < s.length() && isdigit(s[i+1])) {
+                    //sum += sum*10 + s[i+1] - '0'; //WRONG!!!
+                    sum = sum*10 + s[i+1] - '0';
+                    i ++;
+                }
+                res += sum*flag;
+            }
+            else if (s[i] == '+')
+                flag = 1;
+            else if (s[i] == '-')
+                flag = -1;
+            else if (s[i] == '(') {
+                st.push(res);
+                st.push(flag);
+                res = 0;
+                flag = 1;
+            }
+            else if (s[i] == ')') {
+                int a = st.top();//sign
+                st.pop();
+                int b = st.top();
+                st.pop();
+                res = res*a + b;
+            }
+        }
+        return res;
+    }
+};
