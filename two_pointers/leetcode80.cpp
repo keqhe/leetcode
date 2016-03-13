9e251891eb96dc1a80c3c56a02ff9e4517d9746a
keqhe <keqhe@cs.wisc.edu>
2016-03-13 00:03:33 -0600

    80. Remove Duplicates from Sorted Array II

---
+++ two_pointers/leetcode80.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() < 3)
+            return nums.size();
+        
+        int i = 0;
+        int j = 0;
+        int dup = 0;
+        while (j < nums.size()) {
+            if (j > 0 && nums[j] == nums[j-1])
+                dup ++;
+            else
+                dup = 0;//reset dup counter to 0, think about test case [1,1,2,2]
+            if (dup == 2) {
+                while (j > 0 && nums[j] == nums[j-1])
+                    j ++;
+                dup = 0;
+            }  
+            if (j < nums.size())
+                nums[i++] = nums[j++];
+        }
+        return i;
+    }
+};
