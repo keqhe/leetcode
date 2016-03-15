79d77b25f2eb1848e942b7d0a42db99b0a309472
keqhe <keqhe@cs.wisc.edu>
2016-03-15 07:49:48 -0500

    299. Bulls and Cows

---
+++ hashTable/leetcode299.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        unordered_map<char, int> m;
+        int bull = 0;
+        int cow = 0;
+        for (int i = 0; i < secret.length(); i ++) {
+            if (secret[i] == guess[i])
+                bull ++;
+            else 
+                m[secret[i]] ++;
+        }
+        
+        for (int i = 0; i < guess.length(); i ++) {
+            char c = guess[i];
+            if (m.find(c) != m.end()) {
+                //if (m[c] > 0) {
+                if (m[c] > 0 && guess[i] != secret[i]) {
+                    cow ++;
+                    m[c] --;
+                }
+            }
+        }
+        return to_string(bull) + "A" + to_string(cow) + "B";
+    }
+};
